37c1ac5259cb50f36c8de4f0c5c8da7065862baa
sudeeshvs92 <sudeeshvs01@gmail.com>
2017-02-25 23:39:52 +0530

    example program

---
+++ BasicBot.ino
@@ -0,0 +1,28 @@
+
+#include <Motor.h>
+
+Motor bot;
+
+void setup() {
+
+   
+    
+    bot.setControlPins(0, 1, 2, 3,4);
+    bot.setDelay(20);
+    bot.stop();
+    delay(2000);
+}
+
+void loop() {
+    bot.setSpeed(250);
+    bot.setDelay(20);
+  bot.moveForward();
+  delay(2000);
+    bot.turnLeft();
+     delay(2000);
+     
+     bot.turnRight();
+     delay(2000);
+     bot.setSpeed(150);
+   
+}
